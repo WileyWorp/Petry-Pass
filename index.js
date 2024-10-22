830003a5a8775c6cb66109dadf66ef6a8b72883f
Wiley Worp <wileyscheideman@gmail.com>
2024-10-22 14:13:48 -0600

    woohoo

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-header = document.getElementById('headerDIV')
+enterButton = document.getElementById('enterButton')
 
-header.addEventListener('click', function() {
-    document.getElementById('header').textContent = "Hola!"
+enterButton.addEventListener('click', function() {
+    if (document.getElementById('roomInputDIV').style.display = 'block')
+    document.getElementById('roomInputDIV').style.display = 'none';
+    document.getElementById('room').style.display = 'block'
+    document.getElementById('header').textContent = "Room #246b"
 })
