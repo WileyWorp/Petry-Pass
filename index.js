c5329cf3b7e7851e96ce34df6d231f1cf326eb65
Wiley Worp <wileyscheideman@gmail.com>
2024-11-06 14:19:57 -0700

    done?>

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 const enterButton = document.getElementById('enterButton')
 const requestButton = document.getElementById('requestButton')
 
-enterButton.addEventListener('click', function() {
+enterButton.addEventListener('click', function () {
     if (document.getElementById('roomInputDIV').style.display = 'block')
-    document.getElementById('roomInputDIV').style.display = 'none';
-    document.getElementById('room').style.display = 'inline-flex'
-    document.getElementById('header').textContent = "Room #202"
+        document.getElementById('roomInputDIV').style.display = 'none';
+    document.getElementById('room').style.display = 'inline-flex';
 })
 
-requestButton.addEventListener('click', function() {
+requestButton.addEventListener('click', function () {
     alert("Your request has been sent.")
 })
 
-document.getElementById('header').addEventListener('click', function() {
-    location.replace("./tView")
-    // location.replace("https://wileyworp.github.io/Petry-Pass/tView/")
+document.getElementById('header').addEventListener('click', function () {
+    location.replace("https://wileyworp.github.io/Petry-Pass/tView/")
 })
 
 let minutes = 0;
 let seconds = 0;
-let accepted = false;
-export { accepted };
-if (accepted == true) {
-setInterval(function() {
-    seconds += 1
-    if (seconds < 10) {
-        secondsUpdated = "0" + seconds
-    } else {
-        secondsUpdated = seconds
-    }
-    if (seconds == 60) {
-        seconds = 0
-        minutes += 1
-        document.getElementById('timer').innerHTML = minutes + ":" + secondsUpdated
-    } else {
-        document.getElementById('timer').innerHTML = minutes + ":" + secondsUpdated
-    }
-}, 1000)
+let secondsUpdated = seconds;
+var accepted = localStorage.getItem("accepted");
+let timeUpdated = minutes + ":" + secondsUpdated
+if (accepted) {
+    document.getElementById('acceptedTimer').style.display = "block"
+    document.getElementById('requestDIV').style.display = "none"
+    setInterval(function () {
+        seconds += 1
+        if (seconds < 10) {
+            secondsUpdated = "0" + seconds
+        } else {
+            secondsUpdated = seconds
+        }
+        if (seconds == 60) {
+            seconds = 0
+            minutes += 1
+            timeUpdated = minutes + ":" + secondsUpdated
+            document.getElementById('timer').innerHTML = timeUpdated
+            localStorage.setItem("timeUpdated", timeUpdated)
+        } else {
+            timeUpdated = minutes + ":" + secondsUpdated
+            document.getElementById('timer').innerHTML = timeUpdated
+            localStorage.setItem("timeUpdated", timeUpdated)
+        }
+    }, 1000)
 }
--- tView/tView.js
+++ tView/tView.js
@@ -1,22 +1,19 @@
-
-document.getElementById("headerDIV").style.color = "red";
-document.getElementById('header').addEventListener('click', function() {
-    location.replace("/")
-    // location.replace("https://wileyworp.github.io/Petry-Pass/")
+document.getElementById('header').addEventListener('click', function () {
+    location.replace("https://wileyworp.github.io/Petry-Pass/")
 })
 
-document.getElementById('acceptButton').addEventListener('click', function() {
+document.getElementById('acceptButton').addEventListener('click', function () {
     document.getElementById('h1').textContent = "Pass granted to WILEY"
-    setTimeout(function() {
+    setTimeout(function () {
         document.getElementById('h1').textContent = "Awaiting request..."
     }, 2000)
-    accepted = true
+    localStorage.setItem("accepted", "true");
 })
 
-document.getElementById('denyButton').addEventListener('click', function() {
+document.getElementById('denyButton').addEventListener('click', function () {
     document.getElementById('h1').textContent = "Pass denied to WILEY"
-    setTimeout(function() {
+    setTimeout(function () {
         document.getElementById('h1').textContent = "Awaiting request..."
     }, 2000)
-    accepted = false
-})
+    localStorage.removeItem("accepted")
+});
